fc33b7020130ca72d66575a6ae5d91218eaa6ad3
Vildana Panjeta <vpanjeta1@etf.unsa.ba>
2016-04-25 09:10:53 +0600

    izmjena u klasi

--- Poliklinika/Poliklinika/PoliklinikaBAZA/Models/Osoblje.cs
+++ Poliklinika/Poliklinika/PoliklinikaBAZA/Models/Osoblje.cs
@@ -11,11 +11,11 @@ namespace Poliklinika.PoliklinikaBAZA.Models
     {
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public string OsobljeId { get; set; }//primary key u bazi
+        public int OsobljeId { get; set; }//primary key u bazi
         public string Ime { get; set; }//trebati ce za sihronizaciju kasnije
         public string Prezime { get; set; }//naziv restorana
-        public float DatumRodjenja { get; set; }//tekst o restoranu
-        public float DatumZaposlenja { get; set; }//slika restorana
+        public DateTime DatumRodjenja { get; set; }//tekst o restoranu
+        public DateTime DatumZaposlenja { get; set; }//slika restorana
         public float Plata { get; set; }//broj telefona
         public string Username { get; set; }//geografska sirina i duzina gdje se nalazi restoran
         public string Password { get; set; }
