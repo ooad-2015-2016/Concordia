b1c3b9a29dd185c5924cc1d91c4157eb71b1025b
Vildana Panjeta <vpanjeta1@etf.unsa.ba>
2016-04-25 09:07:39 +0600

    klasa osoblje

---
+++ Poliklinika/Poliklinika/PoliklinikaBAZA/Models/Osoblje.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliklinika.PoliklinikaBAZA.Models
+{
+    class Osoblje
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string OsobljeId { get; set; }//primary key u bazi
+        public string Ime { get; set; }//trebati ce za sihronizaciju kasnije
+        public string Prezime { get; set; }//naziv restorana
+        public float DatumRodjenja { get; set; }//tekst o restoranu
+        public float DatumZaposlenja { get; set; }//slika restorana
+        public float Plata { get; set; }//broj telefona
+        public string Username { get; set; }//geografska sirina i duzina gdje se nalazi restoran
+        public string Password { get; set; }
+    }
+}
