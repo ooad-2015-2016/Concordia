2bb2064f533231b6ce759d819d47676551d77536
Vildana Panjeta <vpanjeta1@etf.unsa.ba>
2016-05-13 11:33:58 +0200

    klase igra

---
+++ Igrica/Assets/Scripts/EasyLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class EasyLevel:MonoBehaviour,ILevel
+    {
+        public void odaberiOpciju()
+        {
+            SceneManager.LoadScene("IgraEasy");
+        }
+    }
+}
---
+++ Igrica/Assets/Scripts/Quit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+public class Quit : MonoBehaviour,Assets.Scripts.ILevel
+{
+
+	// Use this for initialization
+	 public void odaberiOpciju () {
+
+        Application.Quit();
+        
+	}
+	
+	// Update is called once per frame
+	
+}
